a8234bf0252e1d9a28295174da02d7dbcfc8b878
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2024-11-22 18:56:09 +0100

    Structure confirmation

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/api/home", (req, res)=> {
+    res.json({message: "Hello world!"});
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server up on port: ${PORT}`);
+})
---
+++ frontend/src/app/types/User.ts
@@ -0,0 +1,7 @@
+export interface UserInterface {
+  name: string;
+  profilePicture: string;
+  bio?: string;
+  followerCount: number;
+  followingCount: number;
+}
