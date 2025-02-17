5df82cd491d5e3ad19af10956ef2c1476993803a
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2025-02-17 13:24:56 +0100

    Component unit test added: Load Circle

--- frontend/src/__tests__/PostListInfinite.test.tsx
+++ frontend/src/__tests__/PostListInfinite.test.tsx
@@ -1,16 +1,64 @@
 import PostListInfinite from "../app/components/posts/PostListInfinite";
-import { render, waitFor } from "@testing-library/react";
+import { render, waitFor, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@testing-library/jest-dom";
 import { getPostsPaginated } from "../app/utils/fetchInfo"; // adjust import if necessary
 
 jest.mock("../app/utils/fetchInfo", () => ({
   getPostsPaginated: jest.fn().mockResolvedValue({
-    data: { posts: [{}, {}, {}], totalPages: 9, currentPage: 1 },
+    data: {
+      posts: [
+        { id: "67b09166ab6da5751c44645e", content: "wtffff" },
+        { id: "67b09166ab6da5751c44645f", content: "test post 2" },
+        { id: "67b09166ab6da5751c446460", content: "test post 3" },
+      ],
+      totalPages: 9,
+      currentPage: 1,
+    },
   }),
 }));
 
+jest.mock("../app/utils/mutations", () => ({
+  useFetchPosts: jest.fn().mockReturnValue({
+    isLoading: false, // Mock isLoading for useFetchPosts
+    isError: false,
+    data: null,
+  }),
+  useFetchInfinitePosts: jest.fn().mockReturnValue({
+    data: {
+      pages: [
+        {
+          posts: {
+            posts: [
+              {
+                id: "67b09166ab6da5751c44645e",
+                content: "wtffff",
+                name: "Scarlet Blue",
+                createdAt: "2025-02-15T13:06:46.694Z",
+                author: "677c6a67e52f37ce5aac3ca5",
+                authorImage:
+                  "https://lh3.googleusercontent.com/a/ACg8ocIEUg7u1BPPSjPLT4hZ8fJTJdimzZbwlhZYzJoQO_W31f937mWy=s96-c",
+              },
+            ],
+          },
+        },
+      ],
+    },
+
+    fetchNextPage: jest.fn(),
+    hasNextPage: true,
+    isFetchingNextPage: false,
+    status: "pending",
+    isLoading: true,
+  }),
+  useDeletePost: jest.fn(), // Add this line to mock useDeletePost
+}));
+
 const queryClient = new QueryClient();
+
+afterEach(() => {
+  queryClient.clear();
+});
 
 test("making sure jest is configured properly", () => {
   expect(true).toBe(true);
@@ -24,4 +72,17 @@ test("PostListInfinite renders without crashing", async () => {
   );
 
   await waitFor(() => expect(container).toBeInTheDocument());
+});
+
+test("PostListInfinite renders LoadCircle while loading", async () => {
+  render(
+    <QueryClientProvider client={queryClient}>
+      <PostListInfinite />
+    </QueryClientProvider>
+  );
+
+  await waitFor(
+    () => expect(screen.getByTestId("load-circle")).toBeInTheDocument(),
+    { timeout: 5000 } // Allow more time for React to process state updates
+  );
 });
