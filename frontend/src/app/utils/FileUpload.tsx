682d6416079ffdef03a30671c940f5bebb004f4f
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2025-02-25 15:47:49 +0100

    Previous multiple images glitch resolved

--- frontend/src/app/utils/FileUpload.tsx
+++ frontend/src/app/utils/FileUpload.tsx
@@ -1,13 +1,6 @@
+import type React from "react";
 import { useState, useRef } from "react";
-
-export const removeImage = (
-  fileToRemove: File,
-  setSelectedFiles: React.Dispatch<React.SetStateAction<File[]>>
-) => {
-  setSelectedFiles((prevFiles) =>
-    prevFiles.filter((file) => file !== fileToRemove)
-  );
-};
+import { Image } from "lucide-react";
 
 const FileUpload = ({ onImagesUploaded }) => {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
@@ -16,9 +9,9 @@ const FileUpload = ({ onImagesUploaded }) => {
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       const newFiles = Array.from(event.target.files);
-      setSelectedFiles((prevFiles) => [...prevFiles, ...newFiles]);
-      // Pass the files to parent for preview
-      onImagesUploaded(newFiles);
+      const updatedFiles = [...selectedFiles, ...newFiles];
+      setSelectedFiles(updatedFiles);
+      onImagesUploaded(updatedFiles);
     }
   };
 
@@ -36,25 +29,14 @@ const FileUpload = ({ onImagesUploaded }) => {
         multiple
         onChange={handleFileChange}
         className="hidden"
+        accept="image/*"
       />
       <button
         onClick={handleSvgClick}
         className="p-2 rounded-full hover:bg-gray-800 transition-colors"
+        title="Add photos"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-5 h-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-          />
-        </svg>
+        <Image className="w-5 h-5" />
       </button>
     </div>
   );
