04d05fab3808a585d75c5ab02a4aa7b0cdb9a1ab
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2025-03-13 21:52:20 +0100

    Comment deleted

--- frontend/src/__tests__/PostListInfinite.test.tsx
+++ frontend/src/__tests__/PostListInfinite.test.tsx
@@ -1,16 +1,16 @@
-import PostListInfinite from "../app/components/posts/PostListInfinite";
-import { render, waitFor, screen } from "@testing-library/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "@testing-library/jest-dom";
-import { getPostsPaginated } from "../app/utils/fetchInfo"; // adjust import if necessary
+import PostListInfinite from '../app/components/posts/PostListInfinite';
+import { render, waitFor, screen } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@testing-library/jest-dom';
+import { getPostsPaginated } from '../app/utils/fetchInfo'; // adjust import if necessary
 
-jest.mock("../app/utils/fetchInfo", () => ({
+jest.mock('../app/utils/fetchInfo', () => ({
   getPostsPaginated: jest.fn().mockResolvedValue({
     data: {
       posts: [
-        { id: "67b09166ab6da5751c44645e", content: "wtffff" },
-        { id: "67b09166ab6da5751c44645f", content: "test post 2" },
-        { id: "67b09166ab6da5751c446460", content: "test post 3" },
+        { id: '67b09166ab6da5751c44645e', content: 'wtffff' },
+        { id: '67b09166ab6da5751c44645f', content: 'test post 2' },
+        { id: '67b09166ab6da5751c446460', content: 'test post 3' },
       ],
       totalPages: 9,
       currentPage: 1,
@@ -18,7 +18,7 @@ jest.mock("../app/utils/fetchInfo", () => ({
   }),
 }));
 
-jest.mock("../app/utils/mutations", () => ({
+jest.mock('../app/utils/mutations', () => ({
   useFetchPosts: jest.fn().mockReturnValue({
     isLoading: false, // Mock isLoading for useFetchPosts
     isError: false,
@@ -31,13 +31,13 @@ jest.mock("../app/utils/mutations", () => ({
           posts: {
             posts: [
               {
-                id: "67b09166ab6da5751c44645e",
-                content: "wtffff",
-                name: "Scarlet Blue",
-                createdAt: "2025-02-15T13:06:46.694Z",
-                author: "677c6a67e52f37ce5aac3ca5",
+                id: '67b09166ab6da5751c44645e',
+                content: 'wtffff',
+                name: 'Scarlet Blue',
+                createdAt: '2025-02-15T13:06:46.694Z',
+                author: '677c6a67e52f37ce5aac3ca5',
                 authorImage:
-                  "https://lh3.googleusercontent.com/a/ACg8ocIEUg7u1BPPSjPLT4hZ8fJTJdimzZbwlhZYzJoQO_W31f937mWy=s96-c",
+                  'https://lh3.googleusercontent.com/a/ACg8ocIEUg7u1BPPSjPLT4hZ8fJTJdimzZbwlhZYzJoQO_W31f937mWy=s96-c',
               },
             ],
           },
@@ -48,7 +48,7 @@ jest.mock("../app/utils/mutations", () => ({
     fetchNextPage: jest.fn(),
     hasNextPage: true,
     isFetchingNextPage: false,
-    status: "pending",
+    status: 'pending',
     isLoading: false,
   }),
   useDeletePost: jest.fn(),
@@ -60,11 +60,11 @@ afterEach(() => {
   queryClient.clear();
 });
 
-test("making sure jest is configured properly", () => {
+test('making sure jest is configured properly', () => {
   expect(true).toBe(true);
 });
 
-test("PostListInfinite renders without crashing", async () => {
+test('PostListInfinite renders without crashing', async () => {
   const { container } = render(
     <QueryClientProvider client={queryClient}>
       <PostListInfinite />
@@ -74,7 +74,7 @@ test("PostListInfinite renders without crashing", async () => {
   await waitFor(() => expect(container).toBeInTheDocument());
 });
 
-test("PostListInfinite renders LoadCircle while loading", async () => {
+test('PostListInfinite renders LoadCircle while loading', async () => {
   render(
     <QueryClientProvider client={queryClient}>
       <PostListInfinite />
@@ -82,6 +82,6 @@ test("PostListInfinite renders LoadCircle while loading", async () => {
   );
 
   await waitFor(() =>
-    expect(screen.getByTestId("load-circle-wrapper")).toBeInTheDocument()
+    expect(screen.getByTestId('load-circle-wrapper')).toBeInTheDocument()
   );
 });
