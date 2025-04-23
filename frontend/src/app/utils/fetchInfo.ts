9f54465d944092870897c503665a90231562dcdb
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2025-04-23 12:27:36 +0200

    Minor tweaks related to comments implementation. Progress push.

--- frontend/src/app/utils/fetchInfo.ts
+++ frontend/src/app/utils/fetchInfo.ts
@@ -1,16 +1,5 @@
 import axios from 'axios';
 
-export interface Post {
-  id: string;
-  author: string;
-  content: string;
-  likes: [string];
-  images: [string];
-  name: string;
-  createdAt: Date;
-  authorImage: string;
-}
-
 export const fetchPosts = async () => {
   const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
   const response = await axios.get(`${serverUrl}/api/post/`);
@@ -18,10 +7,20 @@ export const fetchPosts = async () => {
 };
 
 export async function getPost(id: string) {
+  try {
+    const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+    const response = await axios.get(`${serverUrl}/api/post/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch post:', error);
+    return null;
+  }
+}
+
+export async function getComment(id: string) {
   const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
-  const response = await axios.get(`${serverUrl}/api/post/${id}`);
-  console.log(response.data.post);
-  return response.data.post;
+  const response = await axios.get(`${serverUrl}/api/post/${id}/comment/${id}`);
+  return response.data.comment;
 }
 
 export async function getPostsPaginated(page: number) {
@@ -31,9 +30,7 @@ export async function getPostsPaginated(page: number) {
     const res = await axios.get(`${serverUrl}/api/post/`, {
       params: { page: page, sort: 'createdAt', limit: 5 },
     });
-    console.log('Response:', res); // Log the entire response
     const totalPages = res.data.totalPages;
-    console.log('Total Pages:', totalPages); // Log the total pages
     const hasNext = page < totalPages;
     return {
       nextPage: hasNext ? page + 1 : undefined,
@@ -49,6 +46,35 @@ export async function getPostsPaginated(page: number) {
       nextPage: undefined,
       previousPage: undefined,
       posts: [],
+    };
+  }
+}
+
+export async function getCommentsPaginated(postId: string, page: number) {
+  const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+
+  try {
+    const res = await axios.get(`${serverUrl}/api/post/${postId}/comment`, {
+      params: { page: page, limit: 5 },
+    });
+    const totalPages = res.data.totalPages;
+    const hasNext = page < totalPages;
+    console.log('res data paginated comm', res.data.comments);
+
+    return {
+      nextPage: hasNext ? page + 1 : undefined,
+      previousPage: page > 1 ? page - 1 : undefined,
+      comments: res.data.comments,
+    };
+  } catch (error: Error | any) {
+    console.error(
+      'Error fetching comments:',
+      error.response ? error.response.data : error.message
+    );
+    return {
+      nextPage: undefined,
+      previousPage: undefined,
+      comments: [],
     };
   }
 }
