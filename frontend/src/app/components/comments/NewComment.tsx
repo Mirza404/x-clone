458749f1969f657f9117c05661363b740b60c4f3
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2025-07-07 19:52:41 +0200

    Comment functionality fixed and working

--- frontend/src/app/components/comments/NewComment.tsx
+++ frontend/src/app/components/comments/NewComment.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import LoadingBar from '../ui/CustomLoadBar';
 import { useSession } from 'next-auth/react';
 import classNames from 'classnames';
@@ -6,34 +6,36 @@ import CustomToaster from '../ui/CustomToaster';
 import toast from 'react-hot-toast';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
+import { useParams } from 'next/navigation';
 
 const NewComment = () => {
-  const textareaRef = React.useRef<HTMLTextAreaElement>(null);  
+  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
   const [progress, setProgress] = useState(0);
   const [loading, setLoading] = useState(true);
-  const {data: session}= useSession();
+  const { data: session } = useSession();
   const [content, setContent] = useState('');
   const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+  const queryClient = useQueryClient();
+  const { id } = useParams<{ id: string }>();
+  const email = session?.user?.email || '';
 
   function newCommentMutation() {
-    const queryClient = useQueryClient();
-  
     return useMutation({
       mutationFn: async ({
         postId,
         content,
-        name,
+        email,
       }: {
         postId: string;
         content: string;
-        name: string;
+        email: string;
       }) => {
         const response = await axios.post(
           `${serverUrl}/api/post/${postId}/comment/new`,
           {
             postId,
             content,
-            name,
+            email,
           }
         );
         return response.data;
@@ -52,10 +54,42 @@ const NewComment = () => {
       },
     });
   }
-  
- function useDeleteComment() {
+
+  const mutation = useMutation({
+    mutationFn: async ({
+      postId,
+      content,
+      email,
+    }: {
+      postId: string;
+      content: string;
+      email: string;
+    }) => {
+      const response = await axios.post(
+        `${serverUrl}/api/post/${postId}/comment/new`,
+        {
+          content,
+          email,
+        }
+      );
+      return response.data;
+    },
+    onSuccess: (_, variables) => {
+      toast.success('Comment created successfully');
+      queryClient.invalidateQueries({
+        queryKey: ['infiniteComments', variables.postId],
+      });
+    },
+    onError: (error: any) => {
+      toast.error(
+        error.response?.data?.message || 'Failed to create the comment'
+      );
+    },
+  });
+
+  function useDeleteComment() {
     const queryClient = useQueryClient();
-  
+
     return useMutation({
       mutationFn: async ({
         commentId,
@@ -87,15 +121,12 @@ const NewComment = () => {
     });
   }
 
-
-
-
   const resetTextareaHeight = () => {
     if (textareaRef.current) {
       textareaRef.current.style.height = '28px';
     }
   };
-  
+
   return (
     <>
       <div className="flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm w-[598px] min-h-[116px]">
@@ -133,6 +164,9 @@ const NewComment = () => {
                   )}px`;
                 }
               }}
+              onFocus={resetTextareaHeight}
+              onBlur={resetTextareaHeight}
+              disabled={loading}
             />
             <div className="w-full h-[48px] py-0.5 mt-1.5">
               <div className="flex flex-row w-full h-full items-center justify-between">
@@ -146,7 +180,9 @@ const NewComment = () => {
                         loading || content.trim() === '',
                     }
                   )}
-                //   onClick={() => newCommentMutation.mutate()} // ovdje treba mutaciju za new comment
+                  onClick={() =>
+                    mutation.mutate({ postId: id, content, email })
+                  } // ovdje treba mutaciju za new comment
                   disabled={loading || content.trim() === ''}
                 >
                   Post
