e025d7da7260c716727bed659ea3e867af200378
Mirza404 <mirzaabdulahovic1@gmail.com>
2026-08-14 19:00:23 +0200

    fix: add ConversationsCacheBridge, a single mount point for the message:new cache listener

---
+++ frontend/src/app/components/messages/ConversationsCacheBridge.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { useConversationsCacheBridge } from '../../hooks/useConversations';
+
+export default function ConversationsCacheBridge() {
+  useConversationsCacheBridge();
+  return null;
+}
